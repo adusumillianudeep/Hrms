03fef17636492ceb9db28970dcb26f75281eabac
adusumillianudeep <adusumillianudeep@gmail.com>
2020-01-12 20:23:01 +0530

    UI theme

---
+++ web/HRMS/src/app/Pages/menu.ts
@@ -0,0 +1,17 @@
+export let MENU_ITEM = [
+    {
+        path: 'organization',
+        title: 'Organization',
+        icon: 'Organization',
+        children: [
+            {
+                path: 'organization-generalinfo',
+                title: 'generalinfo'
+            },
+            {
+                path: 'organization-locationinfo',
+                title: 'locationinfo'
+            },
+        ]
+    }
+];
