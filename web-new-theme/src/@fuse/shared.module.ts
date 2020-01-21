e31d82c6fb83541ff29ff33e451a544956445bef
hardik_bavishi <bavishihardik12345@gmail.com>
2020-01-21 10:03:01 +0530

    Added basic layout as per the theme

---
+++ web-new-theme/src/@fuse/shared.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { FuseDirectivesModule } from '@fuse/directives/directives';
+import { FusePipesModule } from '@fuse/pipes/pipes.module';
+import { MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule, MatDividerModule, MatSelectModule, MatTableModule, MatFormFieldModule, MatInputModule, MatDialogModule, MatMenuModule, MatCardModule, MatSnackBarModule, MatPaginatorModule, MatCheckboxModule } from '@angular/material';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+
+        FlexLayoutModule,
+
+        FuseDirectivesModule,
+        FusePipesModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+        MatIconModule,
+        MatDividerModule,
+        MatSelectModule,
+        MatTableModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDialogModule,
+        FlexLayoutModule,
+        MatMenuModule,
+        MatCardModule,
+        MatSnackBarModule,
+        MatPaginatorModule,
+        MatCheckboxModule,
+        FormsModule,
+
+    ],
+    exports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+        MatIconModule,
+        MatDividerModule,
+        MatSelectModule,
+        MatTableModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDialogModule,
+        FlexLayoutModule,
+        MatMenuModule,
+        MatCardModule,
+        MatSnackBarModule,
+        MatPaginatorModule,
+        MatCheckboxModule,
+        FlexLayoutModule,
+
+        FuseDirectivesModule,
+        FusePipesModule
+    ]
+})
+export class FuseSharedModule {
+}
---
+++ web-new-theme/src/app/navigation/navigation.ts
@@ -0,0 +1,121 @@
+import { FuseNavigation } from '@fuse/types';
+
+export const navigation: FuseNavigation[] = [
+    {
+        id: 'my-shortcuts',
+        title: 'My Shortcuts',
+        type: 'item',
+        icon: 'short_text',
+        url: '/dashboard',
+    },
+    {
+        id: 'admin',
+        title: 'Admin',
+        type: 'collapsable',
+        icon: 'account_circle',
+        children: [
+            {
+                id: 'user-management',
+                title: 'User Management',
+                type: 'collapsable',
+                icon: 'people_outline',
+                children: [
+                    {
+                        id: 'users',
+                        title: 'Users',
+                        type: 'item',
+                        url: '/admin/user-management/users'
+                    },
+                    {
+                        id: 'user-roles',
+                        title: 'User Roles',
+                        type: 'item',
+                        url: '/admin/user-management/user-roles'
+                    }
+                ]
+            },
+            {
+                id: 'job',
+                title: 'Job',
+                type: 'collapsable',
+                icon: 'business_center',
+                children: [
+                    {
+                        id: 'job-titles',
+                        title: 'Job Titles',
+                        type: 'item',
+                        url: '/admin/job'
+                    },
+                    {
+                        id: 'salary-components',
+                        title: 'Salary Components',
+                        type: 'item',
+                        url: '/admin/job'
+                    },
+                    {
+                        id: 'pay-grades',
+                        title: 'Pay Grades',
+                        type: 'item',
+                        url: '/admin/job'
+                    },
+                    {
+                        id: 'employment-status',
+                        title: 'Employment Status',
+                        type: 'item',
+                        url: '/admin/job'
+                    },
+                    {
+                        id: 'job-categories',
+                        title: 'Job Categories',
+                        type: 'item',
+                        url: '/admin/job'
+                    },
+                    {
+                        id: 'work-shifts',
+                        title: 'Work Shifts',
+                        type: 'item',
+                        url: '/admin/job'
+                    }
+                ]
+            },
+            {
+                id: 'organization',
+                title: 'organization',
+                type: 'collapsable',
+                icon: 'business',
+                children: [
+                    {
+                        id: 'general-info',
+                        title: 'General Information',
+                        type: 'item',
+                        url: '/admin/general-info'
+                    },
+                    {
+                        id: 'locations',
+                        title: 'Locations',
+                        type: 'item',
+                        url: '/admin/locations'
+                    },
+                    {
+                        id: 'structure',
+                        title: 'Structure',
+                        type: 'item',
+                        url: '/admin/locations'
+                    },
+                    {
+                        id: 'cost-centers',
+                        title: 'Cost Centers',
+                        type: 'item',
+                        url: '/admin/locations'
+                    },
+                    {
+                        id: 'process-eeo-filling',
+                        title: 'Process EEO Filling',
+                        type: 'item',
+                        url: '/admin/locations'
+                    }
+                ]
+            },
+        ]
+    }
+];
